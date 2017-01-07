08d94757b2918a93ac9bde05b6d0c3099c873ee0
userdanny <dannyCL88@gmail.com>
2017-01-07 09:53:07 -0800

    Creando archivo estilo

---
+++ hola.rb
@@ -0,0 +1,18 @@
+#Este es un comentario de una sola linea
+
+=begin
+	Esto es un 
+	comentario
+	multilinea		
+=end
+
+puts "Hola mundo RUBY"
+
+# class FacturaRapida => factura_rapida.rb
+
+a = 1; b = 2; c = 3
+
+varias_lineas = "esto es un linea \ 
+				esta es otra linea"
+
+puts varias_lineas
